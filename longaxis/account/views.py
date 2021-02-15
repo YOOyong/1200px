0ac5bab458fa5ced8dfd3e19788b83418f3c2c22
YOOyong <phg5656@gmail.com>
2021-02-15 18:25:05 +0900

    create account model

--- longaxis/account/views.py
+++ longaxis/account/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic import FormView
+from django.contrib.auth.hashers import check_password, make_password
+from .models import Account
 # Create your views here.
+
+
+
